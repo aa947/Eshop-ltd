5559440d3ae606506542695e378a2504a262da81
Ahmad Ali <aallii300300@gmail.com>
2020-02-15 11:18:00 +0000

    Update keys_dev.js

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    stripePublishableKey:'pk_test_ndmsGOBtHLyzgyfuCQl1l9DE00TcqwLQSE',
-    stripeSecretKey:'sk_test_PXdrZx5jcRrPtPjdusYDNQfh00r8GQhjsM'
+    stripePublishableKey:'',
+    stripeSecretKey:''
   }
